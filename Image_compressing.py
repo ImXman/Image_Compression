a86a49de5dae0866500d4bcd1ece591071a1f290
Yang Xu <35667575+ImXman@users.noreply.github.com>
2019-03-31 17:45:45 -0400

    Add files via upload

--- Image_compressing.py
+++ Image_compressing.py
@@ -9,7 +9,6 @@ import cv2
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from numpy import linalg
 
 ###############################################################################
 ##read the image and flatten it into R,G,B 3-column matrix
@@ -38,7 +37,7 @@ def kmeans(img,k):
         dist=np.empty((img.shape[0],0))
         for i in range(centroid.shape[0]):
             d=img-centroid[i,:]
-            d=linalg.norm(d, axis=1)
+            d=np.linalg.norm(d, axis=1)
             dist=np.column_stack((dist,d))
         cluster=np.argmin(dist,axis=1).tolist()
         df = pd.DataFrame(np.column_stack((img,cluster)))
@@ -51,7 +50,7 @@ def kmeans(img,k):
 
 ###############################################################################
 ##winner-takes-all
-def wta(img,m,e=0.01,epoch=1000,batch=256):
+def wta(img,m,l=0.01,epoch=1000,batch=256):
     
     minv = np.min(img)
     maxv = np.max(img)
@@ -68,44 +67,77 @@ def wta(img,m,e=0.01,epoch=1000,batch=256):
         dist=np.empty((samples.shape[0],0))
         for j in range(centroid.shape[0]):
             d=samples-centroid[j,:]
-            d=linalg.norm(d, axis=1)
+            d=np.linalg.norm(d, axis=1)
             dist=np.column_stack((dist,d))
         cluster=np.argmin(dist,axis=1).tolist()
         df = pd.DataFrame(np.column_stack((samples,cluster)))
         cen = df.groupby([df.iloc[:,-1]], as_index=False).mean()
         for j in range(cen.shape[0]):
-            centroid[int(cen.iloc[int(j),-1]),:]+=e*(cen.iloc[j,:-1].values-\
+            centroid[int(cen.iloc[int(j),-1]),:]+=l*(cen.iloc[j,:-1].values-\
                      centroid[int(cen.iloc[int(j),-1]),:])
     
     dist=np.empty((img.shape[0],0))
     for i in range(centroid.shape[0]):
         d=img-centroid[i,:]
-        d=linalg.norm(d, axis=1)
+        d=np.linalg.norm(d, axis=1)
         dist=np.column_stack((dist,d))
     cluster=np.argmin(dist,axis=1).tolist()
     
     return cluster, centroid
 
 ###############################################################################
-##mean shift
-def mean_shift(img,error=0.01):
+##Kohonen map or self-organizing map
+##this is naive implementation of SOM. 
+##Therefore, it turns out to be inefficient
+##Cautious: decay of learning rate is not considered here
+def kohonen_map(img,k=256,l=0.01,epoch=1000,batch=256):
     
-    kernal=None
-    centroid = np.zeros(img.shape)
-    for i in range(img.shape[0]):
-        new_cen = img[i,:]
-        old_cen = np.zeros((3,1))
-        err = linalg.norm((new_cen-old_cen), axis=1)
-        
-        while err > error:
-            old_cen=new_cen.copy()
-            d=np.power((np.power(abs(img-img[i,:]),2)).sum(),1/2)
-            orders =np.argsort(d).tolist()
-            new_cen = np.dot(img[orders,:],kernal).sum()
-            
-        centroid[i,:]=new_cen
+    net_size=int(np.sqrt(k))
+    m,n = img.shape
+    ##initialize the net
+    minv = np.min(img)
+    maxv = np.max(img)
+    net  = np.random.randint(low=minv,high=maxv, size=(net_size,net_size,n))
+    net = net.astype(np.float32)
+    ##in this project, we use 5X5 gaussian kernal with sigma 1 
+    ##to define the influence. Since it's hard coding here, if you want
+    ##change influence, you may change those numbers (5,2,1,4) below
+    x, y =5,5
+    x, y = np.mgrid[-(x//2):(x//2)+1, -(y//2):(y//2)+1]
+    g = np.exp(-((x/1)**2+(y/1)**2)/2)
+    g = g[2:,2:]
+    ##find BMU and update neuron weights
+    for i in range(epoch):
+        index=np.random.randint(img.shape[0],size=batch)
+        samples = img[index,:]
+        for j in range(samples.shape[0]):
+            ##find BMU
+            d=net-samples[j,:]
+            d=np.linalg.norm(d, axis=2)  
+            bmu = np.where(d==np.min(d))
+            bmu = [bmu[0][0],bmu[1][0]]
+            ##update neuron weight
+            for a in range(net.shape[0]):
+                for b in range(net.shape[1]):
+                    if abs(a-bmu[0])<=2 and abs(b-bmu[1])<= 2:##4 is also hard coding
+                        net[a,b,:]+=l*g[abs(a-bmu[0]),abs(b-bmu[1])]*\
+                        (samples[j,:]-net[a,b,:])
+                        
+    ##assign organized neuron weights to each sample
+    ##first flatten the net
+    centroid=[]
+    for i in range(net.shape[2]):
+        centroid.append(net[:,:,i].flatten())
+    centroid= np.asarray(centroid).T
     
-    return cluster, centroid
+    dist=np.empty((img.shape[0],0))
+    for i in range(centroid.shape[0]):
+        d=img-centroid[i,:]
+        d=np.linalg.norm(d, axis=1)
+        dist=np.column_stack((dist,d))
+    cluster=np.argmin(dist,axis=1).tolist()
+    
+    return net, cluster, centroid
 
 ###############################################################################
 ##quality loss metrics
@@ -120,7 +152,7 @@ def quality_loss(ori_img,cp_img):
     o= np.asarray(o).T
     c= np.asarray(c).T
     loss=o-c
-    loss=linalg.norm(loss, axis=1)
+    loss=np.linalg.norm(loss, axis=1)
         
     return loss
 
@@ -130,6 +162,8 @@ def quality_loss(ori_img,cp_img):
 cluster, centroid = kmeans(img=img_f,k=8)
 ##wta
 cluster, centroid = wta(img=img_f,m=256)
+##SOM
+net, cluster, centroid = kohonen_map(img=img_f,k=256)
 
 img_cp = np.zeros((img_f.shape[0],3))
 for i in range(img_cp.shape[0]):
@@ -141,12 +175,12 @@ for i in range(1,3):
     
 
 rgb=rgb.astype(np.uint8)
-cv2.imwrite("256_compressed_wta.jpg", rgb)
+cv2.imwrite("256_compressed_som.jpg", rgb)
 cv2.imwrite("orignal.jpg", img)
 
 loss = quality_loss(img,rgb)
 pic=sns.distplot(loss)
-pic.get_figure().savefig("loss_256_wta.jpeg",dpi=1200)
+pic.get_figure().savefig("loss_256_som.jpeg",dpi=1200)
         
         
     
